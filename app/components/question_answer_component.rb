d8c9cad5712c3f4bf04f4bf183149a8d28eca280
Colin Hart <wrk.colinhart@gmail.com>
2024-05-20 16:20:19 -0400

    Extract to ViewComponent

---
+++ app/components/question_answer_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class QuestionAnswerComponent < ViewComponent::Base
+  def initialize(question_answer:, current_user:, url: nil)
+    @question_answer = question_answer
+    @current_user = current_user
+    @url = url
+  end
+end
